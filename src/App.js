66e980720364af37b2f8be8d118aa5edab9a8042
Quang Tran <tranvietquang3077@gmail.com>
2020-07-22 01:48:21 +0700

    lasttry

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import ChoiceCard from "./components/ChoiceCard";
+import { CHOICES, getRoundOutcome } from "./utils/index";
+import ChoiceButtons from "./components/ChoiceButtons";
+
+
+
+const choices = {
+  rock:
+    "https://opengameart.org/sites/default/files/forum-attachments/very%20simple%20rock_0.png",
+  paper: "http://pngimagesfree.com/Paper/Thumb/blank-note-paper-free-clipa.png",
+  scissors: "http://www.pngmart.com/files/1/Scissors-PNG-Pic.png"
+};
+
 
 function App() {
+  const [promt, setGamePrompt] = useState("1,2,3, SHOOT !")
+  const [playerChoice, setPlayerChoice] = useState(null);
+  const [computerChoice, setComputerChoice] = useState(null);
+  const [previousWinner, setPreviousWinner] = useState(null);
+  const [gameHistory, setGameHistory] = useState([]);
+
+  const onPlayerChoose = playerChoice => {
+    const [result, compChoice] = getRoundOutcome(playerChoice);
+    const newUserChoice = CHOICES[playerChoice];
+    const newComputerChoice = CHOICES[compChoice];
+    setPlayerChoice(newUserChoice);
+    setComputerChoice(newComputerChoice);
+    if (result === "Victory!") {
+      setPreviousWinner("You");
+    } else if (result === "Defeat!") {
+      setPreviousWinner("Computer");
+    } else {
+      setPreviousWinner("Tie");
+    }
+    setGamePrompt(result);
+    gameHistory.push(result);
+    setGameHistory(gameHistory);
+  };
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row mb-3">
+          <div className="col-md-8 themed-grid-col">
+            <ChoiceCard
+              title="Computer"
+              previousWinner={previousWinner}
+              imgURL={computerChoice && computerChoice.url}
+            />
+            <h1>{prompt}</h1>
+            <ChoiceButtons onPlayerChoose={onPlayerChoose} />
+
+            <ChoiceCard
+              title="You"
+              previousWinner={previousWinner}
+              imgURL={playerChoice && playerChoice.url}
+            />
+          </div>
+          <div className="col-md-4 themed-grid-col">
+            <h3>History</h3>
+            <ul>
+              {gameHistory.map(result => {
+                return <li>{result}</li>;
+              })}
+            </ul>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
