ab650fdefd96b7ad6b5da97536a0394351fcc7f5
Quang Tran <tranvietquang3077@gmail.com>
2020-07-22 02:13:42 +0700

    huhu

--- src/App.js
+++ src/App.js
@@ -6,12 +6,7 @@ import ChoiceButtons from "./components/ChoiceButtons";
 
 
 
-const choices = {
-  rock:
-    "https://opengameart.org/sites/default/files/forum-attachments/very%20simple%20rock_0.png",
-  paper: "http://pngimagesfree.com/Paper/Thumb/blank-note-paper-free-clipa.png",
-  scissors: "http://www.pngmart.com/files/1/Scissors-PNG-Pic.png"
-};
+
 
 
 function App() {
